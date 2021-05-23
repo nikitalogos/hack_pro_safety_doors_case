85f33a68ee8646848632a758bfcff374ed454ecb
safic2 <safic2@yandex-team.ru>
2021-05-23 04:17:06 +0300

    Fine tune algorithm

--- clusterization.py
+++ clusterization.py
@@ -244,7 +244,7 @@ def visualize_plane(points, vectors):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END VIZUALIZATION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def find_platform_floor(points, is_plotting=False):
     points_plane, points_best = hough_planes(
-        points, 1000,
+        points, 500,
         fi_step=0.5, theta_step=0.5,
         theta_bounds=[-10, 10],
         depth_grads=200,
@@ -274,7 +274,7 @@ def find_train_wall(points, is_plotting=False):
     points_plane, points_best = hough_planes(
         points, 500,
         fi_step=0.5, theta_step=0.5,
-        fi_bounds=[80, 100], theta_bounds=[80, 100],
+        fi_bounds=[80, 100], theta_bounds=[80, 95],
         depth_grads=200, depth_start=3,
     )
     if points_plane is None:
@@ -282,7 +282,7 @@ def find_train_wall(points, is_plotting=False):
 
     points_plane2 = []
     for plane in points_plane:
-        if plane[2] < 0:
+        if plane[1] > 0:
             print(f'oops, got inverted vector: {plane}, inverting back to normal')
             plane[:3] *= -1
         points_plane2.append(plane)
@@ -319,7 +319,12 @@ DOOR_STEP_M = 0.1
 DOOR_OPEN_THRESHOLD = 40
 DOOR_FULL_OPEN_M = 0.6
 
+MIN_OBJECT_POINTS = 100
+
 def clusterize(points, is_plotting=False) -> object or None:
+    if is_plotting:
+        show_points(points)
+
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~find floor plane. remove floor points~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     pf_vector = find_platform_floor(points, is_plotting=is_plotting)
     if pf_vector is None:
@@ -341,27 +346,28 @@ def clusterize(points, is_plotting=False) -> object or None:
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~find doors plane. select doors points~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     tw_vector = find_train_wall(points, is_plotting=is_plotting)
-    if tw_vector is None:
-        print('Failed to find_train_wall!')
-        return None
-    tw_vector = tw_vector[:3]
-    if is_plotting:
-        visualize_plane(points, np.expand_dims(tw_vector, axis=0))
+    is_train_wall_found = tw_vector is not None
+    if not is_train_wall_found:
+        print('Train wall not found!')
+    else:
+        tw_vector = tw_vector[:3]
+        if is_plotting:
+            visualize_plane(points, np.expand_dims(tw_vector, axis=0))
 
-    points_proj = projection(points, tw_vector)
-    tw_vec_len = vec_len(tw_vector)
+        points_proj = projection(points, tw_vector)
+        tw_vec_len = vec_len(tw_vector)
 
-    door_mask = (points_proj > (tw_vec_len - DOOR_ZONE_WIDTH_M)) * (points_proj < (tw_vec_len + DOOR_ZONE_WIDTH_M))
-    points_door = points[door_mask]
-    points_proj_door = points_proj[door_mask]
+        door_mask = (points_proj > (tw_vec_len - DOOR_ZONE_WIDTH_M)) * (points_proj < (tw_vec_len + DOOR_ZONE_WIDTH_M))
+        points_door = points[door_mask]
+        points_proj_door = points_proj[door_mask]
 
-    #~~~~~~~~
-    points_mask = points_proj < (tw_vec_len - DOOR_ZONE_WIDTH_M)
-    points_masked = points[points_mask]
+        #~~~~~~~~
+        points_mask = points_proj < (tw_vec_len - DOOR_ZONE_WIDTH_M)
+        points_masked = points[points_mask]
 
-    remove_num = len(points) - len(points_masked)
-    print(f'Removed {remove_num} points beyond the door')
-    points = points_masked
+        remove_num = len(points) - len(points_masked)
+        print(f'Removed {remove_num} points beyond the door')
+        points = points_masked
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PERFORM ANALYSIS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PERFORM ANALYSIS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
@@ -369,63 +375,64 @@ def clusterize(points, is_plotting=False) -> object or None:
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~start door open percent~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     print('~~~~~~~~~~~~~~~~~~~~door open percent~~~~~~~~~~~~~~~~~~~~')
-    if is_plotting:
-        show_points(points_door)
+    if is_train_wall_found:
+        if is_plotting:
+            show_points(points_door)
 
-    tw_vector_norm = vec_norm(tw_vector)
-    proj_vectors = np.zeros((len(points_proj_door), 3))
-    for i in range(3):
-        proj_vectors[:, i] = points_proj_door * tw_vector_norm[i]
-    door_points_zero_plane = points_door - proj_vectors
-    if is_plotting:
-        show_points(door_points_zero_plane)
+        tw_vector_norm = vec_norm(tw_vector)
+        proj_vectors = np.zeros((len(points_proj_door), 3))
+        for i in range(3):
+            proj_vectors[:, i] = points_proj_door * tw_vector_norm[i]
+        door_points_zero_plane = points_door - proj_vectors
+        if is_plotting:
+            show_points(door_points_zero_plane)
 
-    proj_axis = np.cross(tw_vector, pf_vector)
-    proj_axis2 = np.cross(tw_vector, proj_axis)
-    proj_axis2_norm = vec_norm(proj_axis2)
-    door_points_zero_plane_proj = projection(door_points_zero_plane, proj_axis2_norm)
-    print('proj_axis', proj_axis)
-    print('proj_axis2', proj_axis2)
+        proj_axis = np.cross(tw_vector, pf_vector)
+        proj_axis2 = np.cross(tw_vector, proj_axis)
+        proj_axis2_norm = vec_norm(proj_axis2)
+        door_points_zero_plane_proj = projection(door_points_zero_plane, proj_axis2_norm)
+        print('proj_axis', proj_axis)
+        print('proj_axis2', proj_axis2)
 
-    proj_vectors = np.zeros((len(door_points_zero_plane), 3))
-    for i in range(3):
-        proj_vectors[:, i] = door_points_zero_plane_proj * proj_axis2_norm[i]
-    door_points_zero_line = door_points_zero_plane - proj_vectors
-    if is_plotting:
-        show_points(door_points_zero_line)
+        proj_vectors = np.zeros((len(door_points_zero_plane), 3))
+        for i in range(3):
+            proj_vectors[:, i] = door_points_zero_plane_proj * proj_axis2_norm[i]
+        door_points_zero_line = door_points_zero_plane - proj_vectors
+        if is_plotting:
+            show_points(door_points_zero_line)
 
-    dp = projection(door_points_zero_plane, proj_axis)
-    dp.sort()
-    min_val = np.min(dp)
-    max_val = np.max(dp)
-    bin_bounds = np.arange(min_val, max_val, DOOR_STEP_M)
-    bins = []
-    for i in range(len(bin_bounds) - 1):
-        bin_count = np.sum(
-            (dp >= bin_bounds[i]) * (dp < bin_bounds[i + 1])
-        )
-        bins.append(bin_count)
-    bins = np.array(bins)
-    bins_is_open = bins < DOOR_OPEN_THRESHOLD
-    print('bins', bins)
-    print('bins_is_open', bins_is_open)
+        dp = projection(door_points_zero_plane, proj_axis)
+        dp.sort()
+        min_val = np.min(dp)
+        max_val = np.max(dp)
+        bin_bounds = np.arange(min_val, max_val, DOOR_STEP_M)
+        bins = []
+        for i in range(len(bin_bounds) - 1):
+            bin_count = np.sum(
+                (dp >= bin_bounds[i]) * (dp < bin_bounds[i + 1])
+            )
+            bins.append(bin_count)
+        bins = np.array(bins)
+        bins_is_open = bins < DOOR_OPEN_THRESHOLD
+        print('bins', bins)
+        print('bins_is_open', bins_is_open)
 
-    max_open_sequence = 0
-    this_open_sequence = 0
-    for i in range(len(bins_is_open)):
-        bin_is_open = bins_is_open[i]
-        if bin_is_open:
-            this_open_sequence += 1
-        else:
-            max_open_sequence = max(max_open_sequence, this_open_sequence)
-            this_open_sequence = 0
-    max_open_sequence = max(max_open_sequence, this_open_sequence)
-    door_open_width = max_open_sequence * DOOR_STEP_M
-    print('door_open_width', door_open_width)
+        max_open_sequence = 0
+        this_open_sequence = 0
+        for i in range(len(bins_is_open)):
+            bin_is_open = bins_is_open[i]
+            if bin_is_open:
+                this_open_sequence += 1
+            else:
+                max_open_sequence = max(max_open_sequence, this_open_sequence)
+                this_open_sequence = 0
+        max_open_sequence = max(max_open_sequence, this_open_sequence)
+        door_open_width = max_open_sequence * DOOR_STEP_M
+        print('door_open_width', door_open_width)
 
-    door_open_percent = int(door_open_width / DOOR_FULL_OPEN_M * 100)
-    door_open_percent = min(door_open_percent, 100)
-    scene.set_door_open_percent(door_open_percent)
+        door_open_percent = int(door_open_width / DOOR_FULL_OPEN_M * 100)
+        door_open_percent = min(door_open_percent, 100)
+        scene.set_door_open_percent(door_open_percent)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~end door open percent~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~start detect blobs from remaining points~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     print('~~~~~~~~~~~~~~~~~~~~detect objects~~~~~~~~~~~~~~~~~~~~')
@@ -436,7 +443,7 @@ def clusterize(points, is_plotting=False) -> object or None:
     pcd.points = o3d.utility.Vector3dVector(points[:, :3])
     pcd_filtered, _ind = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=0.25)
     print('pcd_filtered', pcd, pcd_filtered)
-    cluster_idxes = pcd_filtered.cluster_dbscan(eps=0.2, min_points=1)
+    cluster_idxes = pcd_filtered.cluster_dbscan(eps=0.1, min_points=1)
     points = np.asarray(pcd_filtered.points)
 
     clusters = {}
@@ -449,7 +456,7 @@ def clusterize(points, is_plotting=False) -> object or None:
 
     keys_to_del = []
     for k,v in clusters.items():
-        if len(v) < 20:
+        if len(v) < MIN_OBJECT_POINTS:
             keys_to_del.append(k)
             continue
         clusters[k] = np.array(clusters[k])
