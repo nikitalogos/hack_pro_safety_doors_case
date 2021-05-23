c8576cc6116ca0c1cbf8e9ab805f3c9748e542d4
dimaPet-ov <71882409+dimaPet-ov@users.noreply.github.com>
2021-05-23 09:09:32 +0300

    Update json_mapped.py

--- json_mapped.py
+++ json_mapped.py
@@ -31,7 +31,6 @@ def text_3d(text, pos, direction=None, degree=0.0, font='arial.ttf', font_size=1
     font_obj = ImageFont.truetype(font, font_size * density)
     font_dim = font_obj.getsize(text)
 
-    
     img = Image.new('RGB', font_dim, color=(255, 255, 255))
     if text=='Train can not move':
         img = Image.new('RGB', font_dim, color=(255, 0, 0))
@@ -244,15 +243,15 @@ def get_box(text):
                 disp.append(txt["geometry"]["dimensions"]["y"])
                 disp.append(txt["geometry"]["dimensions"]["z"])
                 # координаты углов квадрата
-                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]],
+                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
 
-                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
+                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
                           ]
                 lines = [[0, 1], [0, 3], [1, 2], [2, 3],
                          [4, 5], [4, 7], [5, 6], [6, 7],
@@ -280,15 +279,15 @@ def get_box(text):
                 disp.append(txt["geometry"]["dimensions"]["y"])
                 disp.append(txt["geometry"]["dimensions"]["z"])
                 # координаты углов квадрата
-                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]],
+                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
 
-                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
+                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
                           ]
                 lines = [[0, 1], [0, 3], [1, 2], [2, 3],
                          [4, 5], [4, 7], [5, 6], [6, 7],
@@ -316,15 +315,15 @@ def get_box(text):
                 disp.append(txt["geometry"]["dimensions"]["y"])
                 disp.append(txt["geometry"]["dimensions"]["z"])
                 # координаты углов квадрата
-                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]],
+                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
 
-                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
+                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
                           ]
                 lines = [[0, 1], [0, 3], [1, 2], [2, 3],
                          [4, 5], [4, 7], [5, 6], [6, 7],
@@ -352,15 +351,15 @@ def get_box(text):
                 disp.append(txt["geometry"]["dimensions"]["y"])
                 disp.append(txt["geometry"]["dimensions"]["z"])
                 # координаты углов квадрата
-                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]],
+                points = [[init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2]+ disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2]+ disp[2]/2],
 
-                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
-                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]],
-                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]],
+                          [init_cord[0] - disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] - disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1] + disp[1], init_cord[2] - disp[2]/2],
+                          [init_cord[0] + disp[0] / 2, init_cord[1], init_cord[2] - disp[2]/2],
                           ]
                 lines = [[0, 1], [0, 3], [1, 2], [2, 3],
                          [4, 5], [4, 7], [5, 6], [6, 7],
@@ -384,29 +383,10 @@ def get_o3d_FOR(origin=[0, 0, 0],size=1):
     mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size,origin=origin)
     mesh_frame.translate(origin)
     return(mesh_frame)
-def my_rect():
-    points = [[0, 0, 0],
-              [1, 0, 0],
-              [1, 1, 0],
-              [0,1, 0],
 
-              [0, 0, 5],
-              [1, 0, 5],
-              [1, 1, 5],
-              [0, 1, 5],
-              ]
-    lines = [[0, 1], [0, 3], [1, 2], [2, 3],
-             [4, 5], [4, 7], [5, 6], [6, 7],
-             [0, 4], [1, 5], [2, 6], [3, 7]]
-
-    colors = [[0, 1, 0] for i in range(len(lines))]
-    line_set = o3d.geometry.LineSet()
-    line_set.points = o3d.utility.Vector3dVector(points)
-    line_set.lines = o3d.utility.Vector2iVector(lines)
-    line_set.colors = o3d.utility.Vector3dVector(colors)
-    return line_set
 
 ap = ArgumentParser()
+ap.add_argument('-r', '--choose_json', type=int, required=True, help="1-наш json,0-их json")
 ap.add_argument('-i', '--input_pcd', type=str, required=True, help="Input .pcd file")
 ap.add_argument('-p', '--input_json', type=str, required=True, help="Input .json file")
 args = vars(ap.parse_args())
@@ -418,11 +398,6 @@ print(files)
 file_json = args["input_json"]
 print(file_json)
 
-human_id="6bc1d79329bd444c8fb0717e408dd48c"
-wear_id="f8b0fcbf2fd140caa49049729ad18072"
-other_id="dc58658c7ffd4802a1dd62258b4ad985"
-limb_id="e7eb42dd5e0544eb9f667a3915a64700"
-
 #FOR= get_o3d_FOR()
 
 boxes=[]
@@ -433,56 +408,71 @@ vis = o3d.visualization.Visualizer()
     #считываем JSON
     #ТУТ МЫ СЧИТЫВАЕМ JSON ФАЙЛ ПОЛНОСТЬЮ
 with open( file_json, "r", encoding='utf-8') as fel:
-        text = json.load(fel)
-        boxes=get_box(text)
+     text = json.load(fel)
+     if args["choose_json"]==0:
+        for obj in text["objects"]:
+            if obj['classTitle'] == 'human':
+                human_id = obj['key']
+            if obj['classTitle'] == 'wear':
+                wear_id = obj['key']
+            if obj['classTitle'] == 'other':
+                other_id = obj['key']
+            if obj['classTitle'] == 'limb':
+                limb_id = obj['key']
+pcd = o3d.io.read_point_cloud(files)
         #mybox = my_rect()
-        READY=[]
+READY=[]
+if args['choose_json']==1:
+        print('cчитаем наш json')
+        boxes = get_box1(text)
+        lis=''
+        if text['events']==[]:
+            event='unknown'
+        else:
+            for event in text['events']:
+                lis=lis+'/'+event
+        is_can_move=text['is_can_move']
+        if is_can_move==False:
+            chat='Train can not move'
+        else:
+            chat='Train can move'
+        door_open_proc=str(text['door_open_percent'])
+        op=text['door']
 
-       #  lis=''
-       #  if text['events']==[]:
-       #      event='unknown'
-       #  else:
-       #
-       #      for event in text['events']:
-       #          lis=lis+'/'+event
-       #
-       #
-       #  is_can_move=text['is_can_move']
-       #  if is_can_move==False:
-       #      chat='Train can not move'
-       #  else:
-       #      chat='Train can move'
-       #  door_open_proc=str(text['door_open_percent'])
-       #  op=text['door']
-       #
-       #
-       #  chessboard_coord = o3d.geometry.TriangleMesh.create_coordinate_frame(
-       #      size=0.5, origin=[0, 0, 0])
-       # # pcd_1 = text_3d(event, pos=[-1, 0, 0], font_size=350, density=1)
-       #  pcd_1 = text_3d('Door state: ' + op, pos=[-2, 0, 0], font_size=400, density=1)
-       #  if op!='unknown':
-       #      pcd_2 = text_3d('The door is ' + door_open_proc + '% open', pos=[-2, 0, 0.5], font_size=400, density=1)
-       #      pcd_4 = text_3d(chat, pos=[-2, 0, 1.5], font_size=400, density=1)
-       #  else:
-       #      pcd_2 = text_3d(' ', pos=[-2, 0, 0.5], font_size=400, density=1)
-       #      pcd_4 = text_3d(' ', pos=[-2, 0, 1.5], font_size=400, density=1)
-       #  pcd_3 = text_3d('Events: '+lis, pos=[-2, 0, 1.0], font_size=400, density=1)
-
+        chessboard_coord = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.5, origin=[0, 0, 0])
+       # pcd_1 = text_3d(event, pos=[-1, 0, 0], font_size=350, density=1)
+        pcd_1 = text_3d('Door state: ' + op, pos=[-4, 0, 0], font_size=400, density=1)
+        if op!='unknown':
+            pcd_2 = text_3d('The door is ' + door_open_proc + '% open', pos=[-4, 0, 0.5], font_size=400, density=1)
+            pcd_4 = text_3d(chat, pos=[-4, 0, 1.5], font_size=400, density=1)
+        else:
+            pcd_2 = text_3d(' ', pos=[-4, 0, 0.5], font_size=400, density=1)
+            pcd_4 = text_3d(' ', pos=[-4, 0, 2.5], font_size=400, density=1)
+        pcd_3 = text_3d('Events: '+lis, pos=[-4, 0, 1.0], font_size=400, density=1)
 
         #pcd_20 = text_3d('Test-20mm', pos=[0, 0, 0], font_size=20, density=2)
         #ТУТ МЫ СЧИТЫВАЕМ ФАЙЛ ОБЛАКА ТОЧЕК
-        pcd = o3d.io.read_point_cloud (files)
+
        # READY.append(FOR)
         READY.append(pcd)
         #READY.append(mybox)
         for j in range(len(boxes)):
             READY.append(boxes[j])
-
        # READY.append(pcd_1)
-       #  READY.append(pcd_1)
-       #  READY.append(pcd_2)
-       #  READY.append(pcd_3)
-       #  READY.append(pcd_4)
+        READY.append(pcd_1)
+        READY.append(pcd_2)
+        READY.append(pcd_3)
+        READY.append(pcd_4)
+        o3d.visualization.draw_geometries(READY,
+                                              width=1024,
+                                              height=980)
+if args['choose_json']==0:
+        print('cчитаем их json')
+        boxes = get_box(text)
+        READY.append(pcd)
+        for j in range(len(boxes)):
+            READY.append(boxes[j])
         o3d.visualization.draw_geometries(READY,
                                               width=1024,
                                               height=980)
